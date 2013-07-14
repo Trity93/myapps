03ce76ac5c92ca5691123d322599ff642dfca989
dacer <dacerfeedback@gmail.com>
2013-07-14 21:07:57 +0800

    labs

--- URL2Yourbox/index.js
+++ URL2Yourbox/index.js
@@ -1,5 +1,6 @@
 function onClickDownload() {
-    options = {
+	if (document.getElementById("url").value !== '') {
+		options = {
         files: [
             {
                 'url': document.getElementById("url").value,
@@ -9,6 +10,7 @@ function onClickDownload() {
             // console.log("success");
             document.getElementById('light').style.display='none';
             document.getElementById('fade').style.display='none';
+			showToast("Download finished");
         },
         progress: function(progress) {
             // console.log(progress);
@@ -16,13 +18,19 @@ function onClickDownload() {
         },
         cancel: function() {
             console.log("cancel");
+			showToast("Download canceled");
         },
         error: function(errmsg) {
             console.log(errmsg);
-        }
-    }
-    console.log(document.getElementById("url").value);
-    Dropbox.save(options);
+            showToast(errmsg);
+        }	 
+    }	
+	    console.log(document.getElementById("url").value);
+	    Dropbox.save(options);
+	}else{
+		alert("File URL cannot empty");
+	};
+    
 }
 
 function showProcess(){
@@ -51,5 +59,50 @@ function showProcess(){
 	};
 	var target = document.getElementById('light');
 	var spinner = new Spinner(opts).spin(target);
+}
 
+function showToast(toastText){
+	var allAlpha = 0;
+	var toastDiv = document.createElement("div");
+	toastDiv.setAttribute("id", "toast");
+	toastDiv.style.width = "100%";
+	toastDiv.style.position = "fixed";
+	toastDiv.style.bottom = "35px";
+	toastDiv.style.textAlign = "center";
+
+	var toastInner = document.createElement("span");
+	toastInner.setAttribute("id", "toastInner");
+	toastInner.style.backgroundColor = "black";
+	toastInner.style.color = "white";
+	toastInner.style.padding = "5px 15px";
+	toastInner.style.fontSize = "20px";
+	toastInner.style.borderRadius = "3px";
+	toastInner.style.boxShadow = "3px 3px 6px black";
+
+
+	var node = document.createTextNode(toastText);
+	toastInner.appendChild(node);
+	toastDiv.appendChild(toastInner);
+	var body = document.getElementsByTagName("body")[0];
+	body.appendChild(toastDiv);
+
+	
+	var toastAnim = setInterval(function(){	
+		toastInner.style.opacity = allAlpha;
+		allAlpha += 0.008;
+		if (allAlpha > 0.85) {
+			clearInterval(toastAnim);
+		};
+	},10);
+
+// hideToast
+	setTimeout(function(){
+		var hideToastAnim = setInterval(function(){	
+		toastInner.style.opacity = allAlpha;
+		allAlpha -= 0.008;
+		if (allAlpha <= 0) {
+			clearInterval(hideToastAnim);
+		};
+	},10);
+	},5000);
 }
