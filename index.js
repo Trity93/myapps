8250587c73abd79998c17df55d8f00668d983869
dacer <dacerfeedback@gmail.com>
2013-07-21 15:36:36 +0800

    add update log

--- index.js
+++ index.js
@@ -1,9 +1,88 @@
-function onQRClick(){
-	var myWindow = window.open('','MsgWindow','width=200,height=100');
-	myWindow.document.write("<button onmouseover=\"firstPClick()\">Click me!! width="+screen.width+"height"+screen.height+"</p><script>function firstPClick(){window.moveTo(555,555);}</script>");
-	myWindow.moveTo(333,333);
+function mOver(obj){
+	showUpdateLog();
 }
 
-function onClick(){
-	alert("clicked");
+function exitOnClick(){
+	hideUpdateLog();
 }
+
+
+var mPadding = 0;
+var whatsNewHeight = 0;
+
+function showUpdateLog(){
+	var c=document.getElementById("new-exit");
+	var whatsNew=document.getElementById("whats-new");
+	var newInner=document.getElementById("new-inner");
+	// What's new
+	whatsNew.style.display="block";
+	if (mPadding<30) {
+		var addNewPaddingAnim=window.setInterval(
+		function (){
+			whatsNew.style.padding=mPadding+"px 20%";
+			mPadding++;
+			if(mPadding>3){
+				//new inner
+				newInner.style.display="block";
+			}
+			if(mPadding>30){
+			clearInterval(addNewPaddingAnim);
+			}
+		},10);
+	};
+	
+	
+	//What's new
+	if (whatsNewHeight<300) {
+		var addHeightAnim=window.setInterval(
+		function (){
+			whatsNew.style.height=whatsNewHeight+"px";
+			whatsNewHeight+=3;
+			if(whatsNewHeight>300){
+			clearInterval(addHeightAnim);
+			//show exit Button
+			c.style.display="block";
+			var ctx=c.getContext("2d");
+			ctx.beginPath();
+			ctx.fillStyle="#FFFFFF"
+			ctx.arc(25,25,25,0,2*Math.PI);
+			ctx.fill();
+			ctx.fillStyle="#000000"
+			ctx.font="25px Arial";
+			ctx.fillText("X",17,35);
+			}
+		},10);
+	};
+}
+
+function hideUpdateLog(){
+	var c=document.getElementById("new-exit");
+	var whatsNew=document.getElementById("whats-new");
+	var newInner=document.getElementById("new-inner");
+
+	//What's new
+	//hide exit Button
+	c.style.display="none";
+
+	if (whatsNewHeight>0) {
+		var addHeightAnim=window.setInterval(
+		function (){
+			whatsNew.style.height=whatsNewHeight+"px";
+			whatsNewHeight-=3;
+			if(whatsNewHeight<0){
+				clearInterval(addHeightAnim);
+				newInner.style.display="none";
+				whatsNew.style.display="none";
+				mPadding=0;
+				whatsNew.style.padding=mPadding+"px 20%";
+			}
+
+		},1);
+	};
+
+}
+
+
+
+
+
