3581121e44ac5603727104e5f2d31b69d4bab143
dacer <qqwerrd@gmail.com>
2013-10-14 22:53:07 +0900

    a

--- en/index.js
+++
@@ -1,9 +0,0 @@
-function onQRClick(){
-	var myWindow = window.open('','MsgWindow','width=200,height=100');
-	myWindow.document.write("<button onmouseover=\"firstPClick()\">Click me!! width="+screen.width+"height"+screen.height+"</p><script>function firstPClick(){window.moveTo(555,555);}</script>");
-	myWindow.moveTo(333,333);
-}
-
-function onClick(){
-	alert("clicked");
-}
